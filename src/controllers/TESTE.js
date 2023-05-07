541412ddfa4cc3da0ee3e37fb2957239f886b748
Luan Golembiewski <luangolembiewski74@gmail.com>
2023-05-06 21:43:08 -0300

    asdasd

--- src/controllers/TESTE.js
+++ src/controllers/TESTE.js
@@ -1,16 +1,20 @@
 const puppeteer = require('puppeteer');
 
 const getTest = async(request,response)=>{
-    console.log("teste")
-    (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    try{
+        (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-    await page.goto('https://facebook.com/');
-    await page.screenshot({path:'exemple.png'})
+        await page.goto('https://facebook.com/');
+        await page.screenshot({path:'exemple.png'})
 
-    await browser.close();
-    })();
+        await browser.close();
+        })();
+        return response.status(200).json({teste:"true"});
+    }catch(e){
+        return response.status(500).json({teste:"false"});
+    }
 }
 
 module.exports = {
